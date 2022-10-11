b1f406cd4813bb1bafd38dbc3f9c9098b7a00574
yanooomm <115118650+yanooomm@users.noreply.github.com>
2022-10-11 22:48:18 +0500

    Add files via upload

---
+++ лаб3-1.cpp
@@ -0,0 +1,23 @@
+﻿/* While20. Дано целое число N(> 0).С помощью операций деления нацело и взятия остатка от деления определить,
+имеется ли в записи числа N цифра «2».Если имеется, то вывести true, если нет — вывести false.*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout.setf(ios::boolalpha);
+	bool a;
+	int n,x=0;
+	cin >> n;
+	while (n)
+	{
+		if (n % 10 == 2)
+			x += 1;
+		n = n / 10;
+	}
+	a = x > 0;
+	cout << a;
+
+}
---
+++ лаб3-2.cpp
@@ -0,0 +1,17 @@
+﻿/*Series12. Дан набор ненулевых целых чисел; признак его завершения — число 0. Вывести количество чисел в наборе.*/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int x, k=0;
+	do
+	{
+		cin >> x;
+		if (x)
+			k += 1;
+		
+	} while (x != 0);
+	cout << k;
+}
